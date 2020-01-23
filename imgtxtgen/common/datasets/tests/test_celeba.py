95ae9b98bf5ce47e759c837fe18b7487149fd6ca
HarikrishnanBalagopal <harikrishmenon@gmail.com>
2020-01-23 16:40:29 +0530

    fixed celeba dataset test, removed image

--- imgtxtgen/common/datasets/tests/test_celeba.py
+++ imgtxtgen/common/datasets/tests/test_celeba.py
@@ -2,11 +2,7 @@
 Tests for CelebA dataset.
 """
 
-import numpy as np
-import matplotlib.pyplot as plt
-
 from torch.utils.data import DataLoader
-from torchvision.utils import make_grid
 from imgtxtgen.common.datasets.celeba import get_celeba_dataset
 
 def test_celeba():
@@ -22,12 +18,3 @@ def test_celeba():
 
     images, _ = next(iter(dataloader))
     assert images.size() == (d_batch, 3, d_image_size, d_image_size)
-
-    image_grid = make_grid(images[:64], padding=2, normalize=True)
-    image_grid = np.transpose(image_grid, (1, 2, 0)) # convert channels first to channels last format.
-
-    plt.figure(figsize=(8, 8))
-    plt.axis("off")
-    plt.title("CelebA training images.")
-    plt.imshow(image_grid)
-    plt.savefig('test_batch_from_celeba_dataset.png')
